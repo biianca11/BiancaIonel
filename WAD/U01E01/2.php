99e062d2071197e5c710f09dc01d4bc8465c98e6
biianca11 <164342630+biianca11@users.noreply.github.com>
2024-09-13 12:12:37 +0200

    Add files via upload

---
+++ WAD/U01E01/2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $var1="4";
+    $var2='Bianca';
+
+    echo "<table border=1 cellspacing=1 cellpadding=1>
+    <tr>  
+       <td>$var1</td> 
+    </tr> 
+    <tr>  
+        <td>$var2</td> 
+    </tr> 
+
+</table>";
+?>
+</body>
+</html>
