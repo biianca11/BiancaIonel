5e92510e9e75439de85772815f21683afda98d2c
biianca11 <biancamhi02@gmail.com>
2024-11-07 13:08:55 +0100

    commit

---
+++ WAD/RT_COOKIES_SESSION.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <form action="Session1.php" method="post">
+        <label for= "Selec" > Seleccione un producto: </label>
+        <select name="T-shirts" id="T-shirt">
+            <option value= "Zara"> Zara </option>
+            <option value= "Bershka"> Bershka </option>
+            <option value= "Pull & Bear"> Pull & Bear </option>
+        </select>
+    </form>
+
+
+
+
+</body>
+</html>
